b566c8d049e24a1c75e4618b556a94aaaddb6d48
Matheus Diógenes <mathdiogenes@gmail.com>
2023-06-20 14:30:37 -0300

    feat: remove of Bootstrap import in index.js front-end

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 import Provider from './context/Provider';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Provider>
-      <App />
-    </Provider>
+      <Provider>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
