522d040f383b3b2e2f5ed0cdeda1a43a9def762a
Matheus Diógenes <mathdiogenes@gmail.com>
2023-06-26 14:58:55 -0300

    fix: redirect to /admin/manage

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -23,7 +23,7 @@ function LogIn() {
       if (user.role === 'customer') {
         history.push('/customer/products');
       } else if (user.role === 'administrator') {
-        history.push('/admin/register');
+        history.push('/admin/manage');
       } else if (user.role === 'seller') {
         history.push('/seller/orders');
       }
